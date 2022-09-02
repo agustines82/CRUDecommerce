bf5490d155a77bf69db10fba6c8e5f208f1522a9
Agustin Sampayo <agustines82@gmail.com>
2022-09-02 19:03:43 -0300

    se validaron todos los inputs del form de la ventana modal de la pagina de administracion con JS

--- js/helpers.js
+++ js/helpers.js
@@ -46,58 +46,74 @@ export function validarContrasenia(input) {
 export function validarCodigo(input) {
     if (input.value.length === 3) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
 export function validarMarca(input) {
     if (input.value.length >= 3 && input.value.length <= 30) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarModelo(input) {
     if (input.value.length >= 3 && input.value.length <= 30) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarPrecio(input) {
     if (input.value >= 1) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarCategoria(input) {
     if (input.value.length >= 3 && input.value.length <= 30) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarDescripcion(input) {
-    if (input.value.length <= 1000) {
+    if (input.value.length >= 1000) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarStock(input) {
     if (input.value >= 1) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 export function validarImagen(input) {
     let imagen = /^(ftp|http|https):\/\/[^ "]+$/;
     if (imagen.test(input.value)) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
