1aea408459fef882f7a82ce6814ec18a50a41ea1
Agustin Sampayo <agustines82@gmail.com>
2022-09-02 11:26:48 -0300

    se corrigio el form de la ventana modal y se modifico la tabla de productos a los efectos de cumplir con lo requerido en la pagina de detalle

---
+++ js/classesDeUsuarios.js
@@ -0,0 +1,63 @@
+class Administrador {
+    constructor(nombreUsuario, email, contrasenia, repetirContrasenia) {
+        this.nombre = nombreUsuario;
+        this.email = email;
+        this.contrasenia = contrasenia;
+        this.repetirContrasenia = repetirContrasenia;
+        this.acceso = true;
+    }
+    // propiedades conmutadas: (getters y setters)
+    set setNombre(nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    set setEmail(nuevoEmail) {
+        this.email = nuevoEmail;
+    }
+    set setContrasenia(nuevaContrasenia) {
+        this.contrasenia = nuevaContrasenia;
+    }
+    set setRepetirContrasenia(nuevoReContrasenia) {
+        this.repetirContrasenia = nuevoReContrasenia;
+    }
+    get getNomre() {
+        return this.nombre;
+    }
+    get getEmail() {
+        return this.email;
+    }
+    get getContrasenia() {
+        return this.contrasenia;
+    }
+    get getRepetirContrasenia() {
+        return this.repetirContrasenia;
+    }
+
+    // metodos de la clase:
+    mostrarDatos() {
+        document.write(`<ul>
+    <li>Nombre: ${this.nombre}</li>
+    <li>Email:${this.email}</li>
+    </ul>`);
+    }
+}
+
+class Invitado extends Administrador {
+    constructor(nombreUsuario, direccion, email, contrasenia, repetirContrasenia) {
+        super(nombreUsuario, email, contrasenia, repetirContrasenia);
+        this.direccion = direccion;
+        this.productosSeleccionados = [];
+    }
+
+    // propiedades conmutadas: (getters y setters)
+    set setDireccion(nuevaDireccion) {
+        this.direccion = nuevaDireccion;
+    }
+    get getDireccion() {
+        return this.direccion;
+    }
+
+    //metodos
+    agregarProducto(producto) {
+        this.productosSeleccionados.push(producto);
+    }
+}
