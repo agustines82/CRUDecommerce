e64ab7d1ac4e2b1f2a88c544cb2ba78cd16a4bf2
Agustin Sampayo <agustines82@gmail.com>
2022-09-09 16:23:49 -0300

    creo la formula para mostrar el link de administracion en la pagina principal al logearse un administrador y se modifica el boton de inicio de seseion en el html

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,32 +1,35 @@
 import { validarEmailModal, validarContrasenia } from "./helpers2.js";
+import { listadoEmailAdmin } from "./usuarioAdmin.js";
 
-//validaciones ventana modal
-
+let formModalLogin = document.getElementById("formModalLogin");
 let emailModal = document.getElementById("emailModal");
 let contraseniaModal = document.getElementById("contraseniaModal");
 
+//eventos
+formModalLogin.addEventListener("submit", mostrarLinkAdmin);
+
+//validaciones ventana modal
 emailModal.addEventListener("blur", () => {
-  validarEmailModal(emailModal);
+    validarEmailModal(emailModal);
 });
 contraseniaModal.addEventListener("blur", () => {
-  validarContrasenia(contraseniaModal);
+    validarContrasenia(contraseniaModal);
 });
 
 //logica de pagina principal, para exponer productos
 
-let listaProductos =
-  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+let listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
 cargaInicialExposicion();
 function cargaInicialExposicion() {
-  if (listaProductos.length > 0) {
-    listaProductos.forEach((itemProducto) => {
-      exponerProducto(itemProducto);
-    });
-  }
+    if (listaProductos.length > 0) {
+        listaProductos.forEach((itemProducto) => {
+            exponerProducto(itemProducto);
+        });
+    }
 }
 function exponerProducto(producto) {
-  let exposicion = document.querySelector("#exposicionProductos");
-  exposicion.innerHTML += ` <aside class="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
+    let exposicion = document.querySelector("#exposicionProductos");
+    exposicion.innerHTML += ` <aside class="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
     <div class="card">
       <img
         src=" ${producto.imagen}"
@@ -44,4 +47,17 @@ function exponerProducto(producto) {
       </div>
     </div>
   </aside>`;
+}
+
+//logica para mostrar link de administrador en el navbar de la pagina principal
+function mostrarLinkAdmin(e) {
+    e.preventDefault();
+    //si el email escrito en el formulario se encuentra en el arreglo de lista de administradores entonces habilito el link administrador
+    let emailBuscado = listadoEmailAdmin.includes((email) => email === emailModal.value);
+    console.log(emailBuscado);
+    if (emailBuscado) {
+        //agrego la etiqueta <a> a la etiqueta <li> del navbar de la pagina principal
+        let linkAdmin = document.querySelector("#mostrarAdmin");
+        linkAdmin.innerHTML = `<a class="nav-link text-light" href="pages/admin.html">Administrador</a>`;
+    }
 }
