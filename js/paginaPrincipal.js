934d6fb8e15bd535c576e5df43a22cc4436275c8
¨Tomas <tomasgabriel1999@hotmail.com>
2022-09-12 19:29:03 -0300

    Agregamos el buscador de productos a la pagina principal

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,79 +1,66 @@
 import { validarEmailModal, validarContrasenia } from "./helpers2.js";
-import { listadoEmailAdmin } from "./usuarioAdmin.js";
 
-let emailModalCargado = [];
-let formModalLogin = document.getElementById("formModalLogin");
+//validaciones ventana modal
+
 let emailModal = document.getElementById("emailModal");
 let contraseniaModal = document.getElementById("contraseniaModal");
 
-//eventos
-formModalLogin.addEventListener("submit", guardarEmail);
-
-//validaciones ventana modal
 emailModal.addEventListener("blur", () => {
-    validarEmailModal(emailModal);
+  validarEmailModal(emailModal);
 });
 contraseniaModal.addEventListener("blur", () => {
-    validarContrasenia(contraseniaModal);
+  validarContrasenia(contraseniaModal);
 });
 
-//codigo para instanciar la ventana modal de inicio de sesion
-const modalLogin = new bootstrap.Modal(document.getElementById("formularioModalLogin"));
-const btnModalLogin = document.getElementById("btnModalLogin");
-btnModalLogin.addEventListener("click", mostrarLogin);
-function mostrarLogin() {
-    modalLogin.show();
-}
-
 //logica de pagina principal, para exponer productos
-let listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
-cargaInicialExposicion();
-function cargaInicialExposicion() {
-    if (listaProductos.length > 0) {
-        listaProductos.forEach((itemProducto) => {
-            exponerProducto(itemProducto);
-        });
-    }
-}
-function exponerProducto(producto) {
-    let exposicion = document.querySelector("#exposicionProductos");
-    exposicion.innerHTML += ` <aside class="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
-    <div class="card">
-      <img
-        src=" ${producto.imagen}"
-        class="card-img-top"
-        alt="${producto.modelo}" />
-   
-      <div class="card-body">
-        <h5 class="card-title">
-            ${producto.modelo}
-        </h5>
-        <p class="lead">$${producto.precio}</p>
-        <button type="button" class="btn btn-dark">
-        <a href="pages/detalleProducto.html" class="card-link text-center">Ver mas</a>
-        </button>
-      </div>
-    </div>
-  </aside>`;
+
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+
+
+let exposicionProducto = listaProductos.forEach((producto) => {
+
+  let exposicion = document.querySelector("#exposicionProductos");
+  return exposicion.innerHTML += ` <aside class="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
+                <div class="card">
+                  <img
+                    src=" ${producto.imagen}"
+                    class="card-img-top"
+                    alt="${producto.modelo}" />
+               
+                  <div class="card-body">
+                    <h5 class="card-title">
+                        ${producto.modelo}
+                    </h5>
+                    <p class="lead">$${producto.precio}</p>
+                    <button type="button" class="btn btn-dark">
+                    <a href="pages/detalleProducto.html" class="card-link text-center">Ver mas</a>
+                    </button>
+                  </div>
+                </div>
+              </aside>`;
+});
+
+//Filtro de busqueda Js
+
+btn.onclick = function(){
+  let buscar = document.getElementById("buscar").value;
+  if (buscar == "Premium"){
+    panel.innerHTML = 
+    "◉Drone DJI Mavic Mini 2 Fly More Combo 3 Baterías Cámara 4K 30fps 10km Estabilizador 3 Ejes";
+  }
+  if(buscar == "Standar"){
+    panel.innerHTML = "◉Drone DJI Mavic Air 2 c/ Cámara 4K 48Mpx HDR Vuelo Seguro APAS 3.0";
+  }
+  if(buscar == "Basico"){
+    panel.innerHTML = "◉Drone DJI Mini SE Fly More Combo 3 Baterías Cámara 2.7K Ultra Liviano";
+  } else{
+    getElementById.buscar.
+    panel.innerHTML = "No se encontro ningun resultado";
+  }
 }
 
-//logica para mostrar link de administrador en el navbar de la pagina principal
-//guardamos el email en una variable
-function guardarEmail(e) {
-    e.preventDefault();
-    if (validarEmailModal(emailModal) && validarContrasenia(contraseniaModal)) {
-        //guardamos el email en el arreglo
-        emailModalCargado.push(emailModal.value);
-    }
-}
-mostrarLinkAdmin();
-function mostrarLinkAdmin() {
-    //buscamos el email cargado en el listado de email de administradores
-    let emailBuscado = listadoEmailAdmin.includes(emailModalCargado[0]);
 
-    if (emailBuscado) {
-        //agrego la etiqueta <a> a la etiqueta <li> del navbar de la pagina principal
-        let linkAdmin = document.querySelector("#mostrarAdmin");
-        linkAdmin.innerHTML = `<a class="nav-link text-light" href="pages/admin.html">Administrador</a>`;
-    }
-}
+
+
+
