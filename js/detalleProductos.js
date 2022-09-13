047345e91a2cff33f1de640f4ce4d03b8620b463
Agustin Sampayo <agustines82@gmail.com>
2022-09-13 15:05:49 -0300

    buscar en el local storge el dron con el codigo del parametro que viene por URL

--- js/detalleProductos.js
+++ js/detalleProductos.js
@@ -1,3 +1,6 @@
 //tomamos el parametro de la URL
 const urlParametro = new URLSearchParams(window.location.search);
 let codigoUrl = urlParametro.get("codigo");
+//buscar en el local storge el dron con el codigo del parametro que viene por URL
+let listaProductos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+let dronBuscado = listaProductos.find((dron) => dron.codigo === codigoUrl);
