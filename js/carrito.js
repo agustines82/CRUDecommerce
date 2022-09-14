03ce7f68674f093c9b88f6e10dec3f5c6afc19eb
Agustin Sampayo <agustines82@gmail.com>
2022-09-14 14:05:08 -0300

    funcion eliminar del carrito y funcion sumar

--- js/carrito.js
+++ js/carrito.js
@@ -36,13 +36,52 @@ function carritoDeCompras(dronCarrito) {
 }
 
 //sumamos el total de productos seleccionados
-const suma = carrito.reduce((acumulador, dron) => {
-    return (acumulador = acumulador + parseFloat(dron.precio));
-}, 0);
-console.log(suma);
+function suma() {
+    const suma = carrito.reduce((acumulador, dron) => {
+        return (acumulador = acumulador + parseFloat(dron.precio));
+    }, 0);
+    return suma;
+}
 
 //dibujamos el total de la suma
 let carritoDroneroSumado = document.getElementById("sumaCarritoDronero");
 carritoDroneroSumado.innerHTML = `
-    <p class="display-3 text-center"> Total a pagar: $${suma}</p>
+    <p class="display-3 text-center"> Total a pagar: $${suma()}</p>
     `;
+
+//opcion de eliminar del carrito
+function eliminarDelCarrito(codigo) {
+    Swal.fire({
+        title: "Tu producto será eliminado",
+        text: "compralo, dale.. necesitamos vender...",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Borrar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            //buscamos el codigo en el arreglo del carrito y lo borramos
+            let carrito2 = carrito.filter((dron) => {
+                return dron.codigo != codigo;
+            });
+            carrito = carrito2;
+            //actualizamos el session storage
+            guardarCarritoEnSessionStorage();
+            //borramos de la pagina web
+            borrarCarrito();
+            //generamos de nuevo la pagina
+            cargaCarrito();
+        }
+    });
+}
+
+function guardarCarritoEnSessionStorage() {
+    sessionStorage.setItem("carritoKey", JSON.stringify(carrito));
+}
+
+function borrarCarrito() {
+    let carritoDronero = document.getElementById("carritoDronero");
+    carritoDronero.innerHTML = "";
+}
